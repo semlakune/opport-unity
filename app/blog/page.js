321d835c178d115e57f6fafbb781529b2cfe0f57
Aji <aji@klob.id>
2023-10-28 19:27:37 +0700

    add blog and explore pages

---
+++ app/blog/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+const Blog = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Blog</h1>
+    </div>
+  )
+}
+
+export default Blog
---
+++ app/explore/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+const Explore = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Explore</h1>
+    </div>
+  )
+}
+
+export default Explore
