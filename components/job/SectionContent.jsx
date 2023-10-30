08933888c114a24a3e33ff25dd6719e9c2abeebe
Aji <aji@klob.id>
2023-10-30 09:25:08 +0700

    refactor filter

--- components/job/SectionContent.jsx
+++ components/job/SectionContent.jsx
@@ -11,13 +11,44 @@ import {Slider} from "@/components/ui/slider";
 import {Input} from "@/components/ui/input";
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import {
-  faDownLeftAndUpRightToCenter,
-  faUpRightAndDownLeftFromCenter
-} from "@fortawesome/pro-duotone-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {categories, jobTypes, workSystems} from "@/lib/constants";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const FilterList = ({ data }) => (
+  <div className={"h-full flex flex-col gap-0"}>
+    {data.map((item, index) => (
+      <label htmlFor={item.value} key={index} className={"flex items-center justify-between gap-2 hover:bg-secondary py-2 px-3 rounded-full cursor-pointer select-none"}>
+        <div className={"flex items-center gap-2"}>
+          <Checkbox id={item.value} />
+          <p>{item.name}</p>
+        </div>
+        <p className={"flex items-center justify-center text-sm font-wotfardRegular bg-primary text-white h-6 w-6 rounded-full"}>{item.count}</p>
+      </label>
+    ))}
+  </div>
+);
+
+const useToggleFilter = (initialState) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const toggleFilter = (filterName) => {
+    setIsOpen(prevState => ({ ...prevState, [filterName]: !prevState[filterName] }));
+  };
+
+  const collapseOrExpandAll = () => {
+    const allOpen = Object.values(isOpen).every(value => value);
+    setIsOpen(state => {
+      const newState = {};
+      for (let key in state) {
+        newState[key] = !allOpen;
+      }
+      return newState;
+    });
+  };
+
+  return [isOpen, toggleFilter, collapseOrExpandAll];
+};
 
 const FilterSection = ({ title, isOpen, toggleOpen, children }) => {
   const contentRef = useRef(null);
@@ -68,191 +99,57 @@ const FilterSection = ({ title, isOpen, toggleOpen, children }) => {
 };
 
 const SectionContent = () => {
-  const categories = [
-    {
-      name: "Development",
-      value: "development",
-      count: 3,
-    },
-    {
-      name: "Data",
-      value: "data",
-      count: 2,
-    },
-    {
-      name: "Accounting",
-      value: "accounting",
-      count: 1,
-    },
-    {
-      name: "Design",
-      value: "design",
-      count: 4,
-    },
-    {
-      name: "Marketing",
-      value: "marketing",
-      count: 5,
-    },
-    {
-      name: "Writer",
-      value: "writer",
-      count: 6,
-    },
-    {
-      name: "Product",
-      value: "product",
-      count: 7,
-    },
-  ]
-  const jobTypes = [
-    {
-      name: "Full Time",
-      value: "full-time",
-      count: 3,
-    },
-    {
-      name: "Part Time",
-      value: "part-time",
-      count: 2,
-    },
-    {
-      name: "Freelance",
-      value: "freelance",
-      count: 1,
-    },
-    {
-      name: "Internship",
-      value: "internship",
-      count: 4,
-    },
-    {
-      name: "Temporary",
-      value: "temporary",
-      count: 5,
-    },
-  ]
-  const workSystems = [
-    {
-      name: "Remote",
-      value: "remote",
-      count: 3,
-    },
-    {
-      name: "On Site",
-      value: "on-site",
-      count: 2,
-    },
-    {
-      name: "Hybrid",
-      value: "hybrid",
-      count: 1,
-    },
-  ]
-
-  const [isFilterOpen, setIsFilterOpen] = useState({
+  const [isFilterOpen, toggleFilter, collapseOrExpandAll] = useToggleFilter({
     location: true,
     category: true,
     jobType: true,
-    salary: true,
-    workSystem: true,
+    salary: false,
+    workSystem: false,
   });
-  const [sliderValue, setSliderValue] = useState([33, 67]);
+  const [sliderValue, setSliderValue] = useState([0, 3]);
 
-  const toggleFilter = (filterName) => {
-    setIsFilterOpen(prevState => ({ ...prevState, [filterName]: !prevState[filterName] }));
-  };
-
-  const collapseOrExpanAll = () => {
-    if (Object.values(isFilterOpen).includes(true)) {
-      setIsFilterOpen({
-        location: false,
-        category: false,
-        jobType: false,
-        salary: false,
-        workSystem: false,
-      });
-    } else {
-      setIsFilterOpen({
-        location: true,
-        category: true,
-        jobType: true,
-        salary: true,
-        workSystem: true,
-      });
-    }
-  }
+  const filters = [
+    { title: "Location", content: <Combobox /> },
+    { title: "Category", content: <FilterList data={categories} /> },
+    { title: "Job Type", content: <FilterList data={jobTypes} /> },
+    {
+      title: "Salary",
+      content: (
+        <div className={"flex flex-col gap-5"}>
+          <div className={"flex justify-between items-center gap-2"}>
+            <Input type={"number"} value={sliderValue[0]} onChange={(e) => setSliderValue((prev) => [e.target.value, prev[1]])} min={1} className={"select-none m-1"} />
+            <Separator className={"w-4"} />
+            <Input type={"number"} value={sliderValue[1]} onChange={(e) => setSliderValue((prev) => [prev[0], e.target.value])} max={100} className={"select-none m-1"} />
+          </div>
+          <Slider defaultValue={sliderValue} value={sliderValue} min={0} max={100} step={1} onValueChange={(e) => setSliderValue(e)} minStepsBetweenThumbs={1} />
+        </div>
+      )
+    },
+    { title: "Work System", content: <FilterList data={workSystems} /> },
+  ]
 
   return (
     <div className={job.content}>
       <div className={"flex flex-col gap-2"}>
-        <div className={"flex justify-between items-center"}>
+        <div className={"flex gap-4 items-center"}>
           <h1 className={"text-sm font-wotfardRegular"}>Filter By</h1>
-          <div className={"text-xs font-wotfardRegular cursor-pointer bg-primary text-white p-1 rounded-full"} onClick={collapseOrExpanAll}>{Object.values(isFilterOpen).includes(true) ? (
-            <PlusCircledIcon className={"cursor-pointer"} />
-          ) : (
+          <div className={"text-xs font-wotfardRegular cursor-pointer"} onClick={collapseOrExpandAll}>{Object.values(isFilterOpen).every(val => val === true) ? (
             <MinusCircledIcon className={"cursor-pointer"} />
+          ) : (
+            <PlusCircledIcon className={"cursor-pointer"} />
           )}</div>
         </div>
         <div className={job.filterContainer}>
-          <FilterSection title="Location" isOpen={isFilterOpen.location} toggleOpen={() => toggleFilter('location')}>
-            <div className={"h-full flex flex-col select-none"}>
-              <Combobox />
-            </div>
-          </FilterSection>
-
-          <FilterSection title="Category" isOpen={isFilterOpen.category} toggleOpen={() => toggleFilter('category')}>
-            <div className={"h-full flex flex-col gap-0"}>
-              {categories.map((category, index) => (
-                <label htmlFor={category.value} key={index} className={"flex items-center justify-between gap-2 hover:bg-primary py-2 px-3 rounded-full cursor-pointer select-none"}>
-                  <div className={"flex items-center gap-2"}>
-                    <Checkbox id={category.value} className={"border-white"} />
-                    <p>{category.name}</p>
-                  </div>
-                  <p className={"flex items-center justify-center text-sm font-wotfardRegular bg-white h-6 w-6 rounded-full text-primary"}>{category.count}</p>
-                </label>
-              ))}
-            </div>
-          </FilterSection>
-
-          <FilterSection title="Job Type" isOpen={isFilterOpen.jobType} toggleOpen={() => toggleFilter('jobType')}>
-            <div className={"h-full flex flex-col gap-0"}>
-              {jobTypes.map((type, index) => (
-                <label htmlFor={type.value} key={index} className={"flex items-center justify-between gap-2 hover:bg-primary py-2 px-3 rounded-full cursor-pointer select-none"}>
-                  <div className={"flex items-center gap-2"}>
-                    <Checkbox id={type.value} className={"border-white"} />
-                    <p>{type.name}</p>
-                  </div>
-                  <p className={"flex items-center justify-center text-sm font-wotfardRegular bg-white h-6 w-6 rounded-full text-primary"}>{type.count}</p>
-                </label>
-              ))}
-            </div>
-          </FilterSection>
-
-          <FilterSection title="Salary" isOpen={isFilterOpen.salary} toggleOpen={() => toggleFilter('salary')}>
-            <div className={"flex flex-col gap-5"}>
-              <div className={"flex justify-between items-center gap-4"}>
-                <Input type={"number"} value={sliderValue[0]} onChange={(e) => setSliderValue((prev) => [e.target.value, prev[1]])} min={1} className={"select-none"} />
-                <Separator className={"w-4"} />
-                <Input type={"number"} value={sliderValue[1]} onChange={(e) => setSliderValue((prev) => [prev[0], e.target.value])} max={100} className={"select-none"} />
-              </div>
-              <Slider defaultValue={sliderValue} value={sliderValue} onValueChange={(e) => setSliderValue(e)} minStepsBetweenThumbs={1} />
-            </div>
-          </FilterSection>
-
-          <FilterSection title="Work System" isOpen={isFilterOpen.workSystem} toggleOpen={() => toggleFilter('workSystem')}>
-            <div className={"h-full flex flex-col gap-0"}>
-              {workSystems.map((work, index) => (
-                <label htmlFor={work.value} key={index} className={"flex items-center justify-between gap-2 hover:bg-primary py-2 px-3 rounded-full cursor-pointer select-none"}>
-                  <div className={"flex items-center gap-2"}>
-                    <Checkbox id={work.value} className={"border-white"} />
-                    <p>{work.name}</p>
-                  </div>
-                  <p className={"flex items-center justify-center text-sm font-wotfardRegular bg-white h-6 w-6 rounded-full text-primary"}>{work.count}</p>
-                </label>
-              ))}
-            </div>
-          </FilterSection>
+          {filters.map(filter => (
+            <FilterSection
+              key={filter.title}
+              title={filter.title}
+              isOpen={isFilterOpen[filter.title.toLowerCase().replace(' ', '')]}
+              toggleOpen={() => toggleFilter(filter.title.toLowerCase().replace(' ', ''))}
+            >
+              {filter.content}
+            </FilterSection>
+          ))}
         </div>
       </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
+          DEFAULT: "var(--color-secondary)",
           foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
