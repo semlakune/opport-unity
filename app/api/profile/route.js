5dc8fef5518224d9a909e25a2d338a2eb011531e
Aji <aji@klob.id>
2023-11-14 16:53:04 +0700

    test NEXT_PUBLIC_VERCEL_URL

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export async function GET(request) {
   try {
-
+    return NextResponse.json({ message: 'Hello world!' })
   } catch (error) {
     return NextResponse.json({ error: error.message })
   }
