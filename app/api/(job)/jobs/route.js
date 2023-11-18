d62cb9702a228a9e7807dc4244fa3a6ed95cc8c7
Aji <aji@klob.id>
2023-11-19 00:13:18 +0700

    try to fix error dynamic

--- app/api/(job)/jobs/route.js
+++ app/api/(job)/jobs/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/db/prisma";
-
+export const dynamic = "force-dynamic"
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
